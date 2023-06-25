4a2e42bb9c86dd77e2de77612f4c6b5d2a62a12e
Stepan <stepan.sharifulin@mail.ru>
2023-06-25 11:19:29 +0500

    Add structur axios

--- src/api/service/Article.ts
+++ src/api/service/Article.ts
@@ -1,17 +1,16 @@
-import $api, {API_URL} from "../http/http";
 import axios from "axios/index";
 
 interface IArticle {
-    link: string
+    url: string[]
 }
 
-// const $api = axios.create({
-//     withCredentials: true,
-//     baseURL: API_URL,
-// });
+const $api = axios.create({
+    withCredentials: true,
+    baseURL: process.env.BASE_URL
+});
 
 export default class Article {
     static async login() {
-        return axios.post('/login', {})
+        return axios.post<IArticle>('/login', {})
     }
 }
