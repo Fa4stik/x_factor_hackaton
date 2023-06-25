82771d4cf3dde11807c31fe0b1619a72bcb5b2c4
Stepan <stepan.sharifulin@mail.ru>
2023-06-25 15:09:03 +0500

    Set api

--- src/api/service/Article.ts
+++ src/api/service/Article.ts
@@ -2,10 +2,12 @@ import axios, { AxiosResponse, AxiosError } from 'axios';
 import {IArticle, IErrorArticle} from "./types";
 import $api from "../http/http";
 
+// API_URL=http://textify_app:8000/api/v1/
+
 export default class Article {
     static async getArticle(url: string): Promise<{data: IArticle | IErrorArticle, isError: boolean}> {
         try {
-            const response: AxiosResponse<IArticle> = await axios.post<IArticle>('http://textify_app:8000/api/v1//article', {url});
+            const response: AxiosResponse<IArticle> = await axios.post<IArticle>('http://textify_app:8000/api/v1/article', {url});
             return { data: response.data, isError: false };
         } catch (error) {
             const axiosError: AxiosError<IErrorArticle> = error as AxiosError<IErrorArticle>;
