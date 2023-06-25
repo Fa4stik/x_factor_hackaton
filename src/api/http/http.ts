a1483c927bc02b9dc94b321119738fa2cfa5bec6
Stepan <stepan.sharifulin@mail.ru>
2023-06-25 15:52:28 +0500

    Edit access for CORS

--- src/api/http/http.ts
+++ src/api/http/http.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL; // set on valid url
-
 const $api = axios.create({
     withCredentials: true,
-    baseURL: "http://textify_app:8000/api/v1/",
+    baseURL: "http://textify_app:8000/api/v1",
+    headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    }
 });
 
 export default $api;
