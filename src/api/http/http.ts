11ecb30236911f0806614b8f00c6727de2b72e05
Stepan <stepan.sharifulin@mail.ru>
2023-06-25 15:13:33 +0500

    Set api

--- src/api/http/http.ts
+++ src/api/http/http.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export const API_URL = process.env.API_URL; // set on valid url
+const API_URL = process.env.API_URL; // set on valid url
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: API_URL,
+    baseURL: "http://textify_app:8000/api/v1/",
 });
 
 export default $api;
